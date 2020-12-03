225a8593df7218e643071b89d63edad2ae501529
Grace <gminnis1@yahoo.com>
2020-12-03 13:13:54 -0600

    Created initial Product model, controller, and repository

---
+++ liftoff-project/src/main/java/org/launchcode/liftoffproject/models/Product.java
@@ -0,0 +1,54 @@
+package org.launchcode.liftoffproject.models;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Product extends AbstractEntity {
+
+    private String type;
+    private String description;
+    private String photo;
+    private boolean organic;
+
+    public Product(String type, String description, String photo, boolean organic) {
+        super();
+        this.type = type;
+        this.description = description;
+        this.photo = photo;
+        this.organic = organic;
+    }
+
+    public Product() {}
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public boolean isOrganic() {
+        return organic;
+    }
+
+    public void setOrganic(boolean organic) {
+        this.organic = organic;
+    }
+}
